5d54fb24eb152786cc6b7ead416dc14914f821a4
An <1512012@student.hcmus.edu.vn>
2018-11-22 22:53:29 +0700

    Tim Kiem - Man hinh 4 - Loc

--- Source/app/src/main/java/com/meu/an/rooboo/MainActivity.java
+++ Source/app/src/main/java/com/meu/an/rooboo/MainActivity.java
@@ -2,12 +2,33 @@ package com.meu.an.rooboo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    //SeekBar priceBooking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_view);
+        setContentView(R.layout.activity_filter_view);
+
+       /* priceBooking = findViewById(R.id.seekbar_pricerange_id);
+
+        priceBooking.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });*/
     }
 }
