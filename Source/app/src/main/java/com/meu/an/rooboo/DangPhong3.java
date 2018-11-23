0db54fb2da77d35c12a46edc52ed45fc0fa85b09
nttri <bem0302@gmail.com>
2018-11-23 14:56:24 +0700

    [UI] add screen 'Dang phong 3'

---
+++ Source/app/src/main/java/com/meu/an/rooboo/DangPhong3.java
@@ -0,0 +1,13 @@
+package com.meu.an.rooboo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DangPhong3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_phong3);
+    }
+}
