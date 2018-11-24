98354709a2460b4a8708f850f95cbb7120e08e8f
An <1512012@student.hcmus.edu.vn>
2018-11-24 17:56:35 +0700

    Fix conflict and add custom for button 'Hoan thanh'

--- Source/app/src/main/java/com/meu/an/rooboo/MainActivity.java
+++ Source/app/src/main/java/com/meu/an/rooboo/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_view);
+        setContentView(R.layout.activity_dang_phong5);
 
        /* priceBooking = findViewById(R.id.seekbar_pricerange_id);
 
