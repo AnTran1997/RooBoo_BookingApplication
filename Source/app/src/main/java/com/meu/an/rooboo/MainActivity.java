d35902ecf87bb6383dc02bb8b3ddc0fff05cac9b
An <1512012@student.hcmus.edu.vn>
2018-11-24 19:32:25 +0700

    [UI] Add items to spinner fields. Consistent layout for orientation

--- Source/app/src/main/java/com/meu/an/rooboo/MainActivity.java
+++ Source/app/src/main/java/com/meu/an/rooboo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dang_phong5);
+        setContentView(R.layout.activity_search_view);
 
        /* priceBooking = findViewById(R.id.seekbar_pricerange_id);
 
@@ -31,28 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });*/
-
-       //Setup spinner for fields: City, District, Type of room
-        Spinner spinner_thanhpho = findViewById(R.id.dangphong5_spinner_thanhpho);
-        ArrayAdapter<String> thanhphoAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.spinnner_datphong5_thanhpho_items));
-        thanhphoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_thanhpho.setAdapter(thanhphoAdapter);
-
-        Spinner spinner_quan = findViewById(R.id.dangphong5_spinner_quan);
-        ArrayAdapter<String> quanAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.spinnner_datphong5_quan_items));
-        quanAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_quan.setAdapter(quanAdapter);
-
-        Spinner spinner_loaiphong = findViewById(R.id.dangphong5_spinner_loaiphong);
-        ArrayAdapter<String> loaiPhongAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.spinnner_datphong5_loaiphong_items));
-        loaiPhongAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_loaiphong.setAdapter(loaiPhongAdapter);
 
     }
 }
